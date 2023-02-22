bb546f2527a412bbfe68ea1c8ca2c9e630338ad1
sanjeev-sharma-mindtree <sanjeev.sharma@mindtree.com>
2023-02-22 17:43:30 +0530

    added theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,14 @@
-import ReactDOM from 'react-dom'
+import { ThemeProvider } from '@emotion/react';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App'
+import { theme } from "./theme"
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
 
-ReactDOM.render(<App />, document.getElementById('root'))
+    </React.StrictMode>
+)
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#cccccc',
+        },
+        secondary: {
+            main: '#edf2ff',
+        },
+    },
+})
